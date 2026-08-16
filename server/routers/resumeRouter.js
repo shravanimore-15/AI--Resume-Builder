465fb78672dc79fbdd6f0df2991cf24e1e0c0cbd
Shravani More <shravanimore182@gmail.com>
2026-08-16 13:34:55 +0530

    Fix job match delete route

--- server/routers/resumeRouter.js
+++ server/routers/resumeRouter.js
@@ -18,18 +18,37 @@ const {
   deleteJobMatchController,
   getJobMatchByIdController,
 } = require("../controllers/resumeController");
-resumeRouter.post("/",authMiddleware,createResume);
-resumeRouter.get("/",authMiddleware,getAllResume);
-resumeRouter.post("/generate-summary",authMiddleware,generateSummaryController);
-resumeRouter.post("/generate-project-description",authMiddleware,generateProjectDescriptionController);
-resumeRouter.post("/:id/analyze",authMiddleware,analyzeResumeController);
-resumeRouter.post("/:id/job-match",authMiddleware,analyzeJobMatchController);
-resumeRouter.get("/:id/job-matches",authMiddleware,getJobMatchHistoryController);
-resumeRouter.delete("/job-match/:jobMatchId",authMiddlewaredeleteJobMatchController);
-resumeRouter.get("/job-match/:jobMatchId",authMiddleware,getJobMatchByIdController);
-resumeRouter.get("/:id",authMiddleware,getResumeById);
-resumeRouter.put("/:id",authMiddleware,updateResume);
-resumeRouter.delete("/:id",authMiddleware,deleteResume);
-
+resumeRouter.post("/", authMiddleware, createResume);
+resumeRouter.get("/", authMiddleware, getAllResume);
+resumeRouter.post(
+  "/generate-summary",
+  authMiddleware,
+  generateSummaryController,
+);
+resumeRouter.post(
+  "/generate-project-description",
+  authMiddleware,
+  generateProjectDescriptionController,
+);
+resumeRouter.post("/:id/analyze", authMiddleware, analyzeResumeController);
+resumeRouter.post("/:id/job-match", authMiddleware, analyzeJobMatchController);
+resumeRouter.get(
+  "/:id/job-matches",
+  authMiddleware,
+  getJobMatchHistoryController,
+);
+resumeRouter.delete(
+  "/job-match/:jobMatchId",
+  authMiddleware,
+  deleteJobMatchController,
+);
+resumeRouter.get(
+  "/job-match/:jobMatchId",
+  authMiddleware,
+  getJobMatchByIdController,
+);
+resumeRouter.get("/:id", authMiddleware, getResumeById);
+resumeRouter.put("/:id", authMiddleware, updateResume);
+resumeRouter.delete("/:id", authMiddleware, deleteResume);
 
 module.exports = resumeRouter;
