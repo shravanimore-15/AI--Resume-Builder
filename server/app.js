4901fa042ef6b49caf0fd2ccfb12bd16ecd84b19
Shravani More <shravanimore182@gmail.com>
2026-08-16 14:19:16 +0530

    Allow Vercel frontend through CORS

--- server/app.js
+++ server/app.js
@@ -3,22 +3,24 @@ const cors = require("cors");
 require("dotenv").config();
 const authRouter = require("./routers/authRouter");
 const resumeRouter = require("./routers/resumeRouter");
-const userRouter = require("./routers/userRouter")
-
+const userRouter = require("./routers/userRouter");
 
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://ai-resume-builder-bay-nu.vercel.app",
+    ],
     credentials: true,
-  })
+  }),
 );
 
 app.use(express.json());
 app.use("/auth", authRouter);
 app.use("/resume", resumeRouter);
-app.use("/user", userRouter)
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Your backend is running");
