2e3d87f5ae57bd6c74bfb7751ab8cf9e30bc75f5
Shravani More <shravanimore182@gmail.com>
2026-08-16 14:08:42 +0530

    Fix file import paths for production build

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,23 +9,64 @@ import Dashboard from "./pages/Dashboard";
 import "./App.css";
 import Login from "./pages/Login";
 import ResumeView from "./pages/ResumeView";
-
-import ProtectedRoute from "./component/ProtectedRoute";
+import ProtectedRoute from "./component/protectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes><Route path="/"element={<Home />}/>
-       <Route path="/login" element={<Login />}/>
-       <Route path="/signup"element={<SignUp />}/>
-       <Route path="/template"element={<Template />}/>
-       <Route path="/createresume"element={<ProtectedRoute><CreateResume /></ProtectedRoute>}/> 
-       <Route path="/dashboard"element={<ProtectedRoute><Dashboard /></ProtectedRoute>}/>
-       <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>}/>
-       <Route path="/resume"element={<ProtectedRoute><Dashboard /></ProtectedRoute>}/>
-       <Route path="/resumeview/:id"element={<ProtectedRoute><ResumeView /></ProtectedRoute>}/>
-       <Route path="/resume/edit/:id"element={<ProtectedRoute><CreateResume /></ProtectedRoute>}/>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/template" element={<Template />} />
+        <Route
+          path="/createresume"
+          element={
+            <ProtectedRoute>
+              <CreateResume />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/resume"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/resumeview/:id"
+          element={
+            <ProtectedRoute>
+              <ResumeView />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/resume/edit/:id"
+          element={
+            <ProtectedRoute>
+              <CreateResume />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
